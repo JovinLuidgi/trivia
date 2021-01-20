1028ac6351a6868dd44322e5157a84001d527961
JovinLuidgi <43542508+JovinLuidgi@users.noreply.github.com>
2021-01-20 18:18:35 -0500

    RenderQuestion we completed step one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
 import React, { Component, useState } from "react";
+import { getLateBoundPropertyNames } from "tsutils";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-function App() {
-  return <div className="app">Trivia!</div>;
+function Question(props){
+  return <p>{props.question.text}</p>;
+  
 }
+
+function App() {
+  let questionNum = 0;
+
+ return  <div className="app">
+           {data.map(displayQuestion => <Question question={displayQuestion.question}/>)} 
+          </div>;
+  }
 
 export default App;
