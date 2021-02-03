54582c49060441c2604b81d01a74ad08e80505e2
JovinLuidgi <43542508+JovinLuidgi@users.noreply.github.com>
2021-02-03 18:19:08 -0500

    Day 2 - Line 48 Render a button that reveals answer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,43 @@
 import React, { Component, useState } from "react";
 import { getLateBoundPropertyNames } from "tsutils";
 import "../css/App.css";
-import data from "../sample_data.json";
+import questions from "../sample_data.json";
 
-function Question(props){
+function Question(props) {
   return <p>{props.question.text}</p>;
-  
+}
+
+function NextQuestion(props) {
+  return <button onClick={props.handleClick}> next question </button>;
+}
+
+function Answer(props) {
+  return <p>{props.answer}</p>;
 }
 
 function App() {
   let questionNum = 0;
+  let [viewAnswer, setViewAnswer] = useState(false);
 
- return  <div className="app">
-           {data.map(displayQuestion => <Question question={displayQuestion.question}/>)} 
-          </div>;
-  }
+  return (
+    <div className="app">
+      {questions.map((displayQuestion) => (
+        <div>
+          <Question question={displayQuestion.question} />
+          <NextQuestion handleClick={() => setViewAnswer(!viewAnswer)} />
+          {viewAnswer && (
+            <Answer
+              answer={
+                displayQuestion.question.choices[
+                  displayQuestion.question.correct_choice_index
+                ]
+              }
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default App;
